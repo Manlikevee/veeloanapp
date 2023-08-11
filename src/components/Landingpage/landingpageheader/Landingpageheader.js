9a8b996c95685b5e0623e878fd1de8f24ebaf1a1
manlikevee <odahviktor@gmail.com>
2023-08-11 17:49:54 +0100

    n

--- src/components/Landingpage/landingpageheader/Landingpageheader.js
+++ src/components/Landingpage/landingpageheader/Landingpageheader.js
@@ -1,81 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import logo from "../logo (2).png";
-import { Link } from "gatsby";
+import React, { useState } from 'react';
+import logo from "../logo (2).png"
+import { Link } from "gatsby"
 import { Helmet } from 'react-helmet';
-
 const Landingpageheader = () => {
+
   const [isActive, setIsActive] = useState(false);
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
 
   const handleClick = () => {
+
     setIsActive(!isActive);
   };
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleWindowResize = () => {
-        setIsLargeScreen(window.innerWidth > 900);
   
-        if (window.innerWidth <= 900) {
-          setIsActive(false);
-        }
-      };
-  
-      window.addEventListener('resize', handleWindowResize);
-      handleWindowResize();
-  
-      return () => {
-        window.removeEventListener('resize', handleWindowResize);
-      };
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsLargeScreen(window.innerWidth > 900);
-    }
-  }, []);
-
   return (
-    <>
-      <Helmet>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-        <meta name="description" content="This is a description of my page." />
-        {/* Add more head tags here */}
-      </Helmet>
-      <div id="navlinkovalay" className={`target-div ${isActive ? 'navlinkactiveovalay' : ''}`} />
-      <header id="navbar" className='mynav'>
-        <div className="container" data-aos="fade-left">
-          <Link to='' className="logo">
-            <img src={logo} alt="" />
-          </Link>
-          <div className={`target-div ${isActive ? 'navlinkactive' : 'links'}`} id="middlelinks">
-            <Link to='' className="activated">
-              Home
-            </Link>
-            <Link to='/'>About </Link>
-            <Link to='/'>What We Do </Link>
-            <Link to='/'>Contact Us </Link>
-            <div className="hideme">
-              <Link to='/Userdashboard' className="blackboton">
-                {" "}
-                Get Started
-              </Link>
-            </div>
-          </div>
-          <Link to='/Userlogin'
-            className="login getstarteds"
-            style={{ width: "fit-content" }}
-          >
-            Get Started
-          </Link>
-          <div className={`navtoggle ${isLargeScreen ? '' : 'inactive'}`} onClick={handleClick}>
-            <span className="material-symbols-outlined">menu</span>
-          </div>
-        </div>
-      </header>
-    </>
-  );
-};
 
-export default Landingpageheader;
+    <>      
+    <Helmet>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  
+    <meta name="description" content="This is a description of my page." />
+    {/* Add more head tags here */}
+  </Helmet>   
+  <div id="navlinkovalay" className={`target-div ${isActive ? 'navlinkactiveovalay' : ''}`} />
+   <header id="navbar" className='mynav'>
+    <div className="container" data-aos="fade-left">
+    <Link to='' className="logo" >
+        <img src={logo} alt="" />
+      </Link>
+      <div  className={`target-div ${isActive ? 'navlinkactive' : 'links'}`} id="middlelinks">
+        <Link to=''  className="activated">
+          Home
+        </Link>
+        <Link  to='/'>About </Link>
+        <Link  to='/'>What We Do </Link>
+        <Link  to='/'>Contact Us </Link>
+        <div className="hideme">
+        <Link  to='/Userdashboard' className="blackboton">
+            {" "}
+            Get Started
+            </Link>
+        </div>
+      </div>
+     <Link to='/Userlogin'
+        className="login getstarteds"
+        style={{ width: "fit-content" }}
+        
+      >
+        Get Started
+      </Link>
+      <div className="navtoggle" onClick={handleClick}>
+        <span className="material-symbols-outlined">menu</span>
+      </div>
+    </div>
+  </header>
+  </>
+
+  )
+}
+
+export default Landingpageheader
