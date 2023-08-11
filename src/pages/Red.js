93b0b00dff2d5b1dcb46e3a19917e9a88bfa8cc8
manlikevee <odahviktor@gmail.com>
2023-08-11 17:55:45 +0100

    vv

--- src/pages/Red.js
+++ src/pages/Red.js
@@ -1,27 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import "../components/Landingpage/layout.css";
-const MyComponent = () => {
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 900);
+import React from 'react'
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setIsLargeScreen(window.innerWidth > 900);
-    };
-
-    // Attach the resize event listener
-    window.addEventListener('resize', handleWindowResize);
-
-    // Cleanup the event listener when component unmounts
-    return () => {
-      window.removeEventListener('resize', handleWindowResize);
-    };
-  }, []); // Empty dependency array to run the effect once
-
+const Red = () => {
   return (
-    <div className={`box ${isLargeScreen ? 'red' : 'blue'}`}>
-      {/* Your component's content */}
-    </div>
-  );
-};
+    <div>Red</div>
+  )
+}
 
-export default MyComponent;
+export default Red
