c26f77dbe4d570b56e376d7a9ff2f65188128d14
manlikevee <odahviktor@gmail.com>
2023-08-16 18:07:41 +0100

    vic

--- client/src/components/Dashboard/Loanpopup/Loanpopup.js
+++ client/src/components/Dashboard/Loanpopup/Loanpopup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ButtonComponent from '../../Buttoncomponent'
+import ButtonComponent from '../ButtonComponent'
 
 const Loanpopup = ({ openModal }) => {
   return (
