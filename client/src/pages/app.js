57845e1f814088e17a833a119f7a059ede5d0971
manlikevee <odahviktor@gmail.com>
2023-08-22 18:28:24 +0100

    victor

--- client/src/pages/app.js
+++ client/src/pages/app.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Router } from "@reach/router"
-import Userprofile from "./privateroute/Userprofile"
+import Userprofile from "../privateroute/Userprofile"
 import PrivateRoute from "../components/privateRoute"
 import Userlogin from "./Userlogin"
 
