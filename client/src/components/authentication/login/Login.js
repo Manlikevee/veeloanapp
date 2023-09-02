51366d5e2c0a205b1d18143b8467ad5956be6290
manlikevee <odahviktor@gmail.com>
2023-09-02 23:30:16 +0100

    vee

--- client/src/components/authentication/login/Login.js
+++ client/src/components/authentication/login/Login.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { Link } from "gatsby"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Helmet } from 'react-helmet';
 import image from "../logo (2).png"
 import Splash from '../splashscreen/Splash'
+
+
+
 function Login({ children, typeofpage }) {
+  <Helmet>
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+<meta name="description" content="This is a description of my page." />
+{/* Add more head tags here */}
+</Helmet>  
   return (
     <div>
           <ToastContainer/>
--- client/src/pages/Emailverificationdone.js
+++ client/src/pages/Emailverificationdone.js
@@ -7,8 +7,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import Login from '../components/authentication/login/Login';
 import { navigate } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
 
 const Emailverificationdone = () => {
+
+  <Helmet>
+  <link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  
+  <meta name="description" content="This is a description of my page." />
+  {/* Add more head tags here */}
+  </Helmet>  
+
   const [Verificationtoken, SetVerificationtoken] = useState('');
   const [loading, setLoading] = useState(true);
   const [responseData, setResponseData] = useState(null);
@@ -42,7 +53,12 @@ const Emailverificationdone = () => {
           // Handle the response as needed
           console.log('Verification successful', response.data);
           setResponseData(response.data);
-          setLoading(false);
+
+          setTimeout(() => {
+            setLoading(false);
+          }, 4000);
+
+          
         })
         .catch(error => {
           // Handle errors
@@ -56,11 +72,49 @@ const Emailverificationdone = () => {
   // Conditional rendering based on responseData
   let content;
   if (loading) {
-    content = <p>Loading...</p>;
+    content = <div>
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+    <div className="logintitle">
+    Verification in progress. Please wait while we complete the verification process. This may take a moment. Thank you for your patience.
+        </div>
+
+    </div>;
   } else if (responseData.message) {
-    content = <p> {responseData.message}</p>;
+    content = <div> {responseData.message} 🎉
+
+<div className="logintitle">
+Congratulations! Your email address has been successfully verified, and your registration is now complete. You can now access our services and enjoy all the benefits of our platform.
+    <div>Thank you for choosing us! </div>
+    
+        </div>
+
+</div>;
   } else if (responseData && responseData.error) {
-    content = <p>Verification failed. {responseData.error}</p>;
+    content = <div>
+      {responseData.error === 'Your account is already verified.' ? (
+  // Display something when the error message is 'Your account is already verified'
+  <div>
+ <div className='mytitle'> {responseData.error} </div>
+ <div> 
+  <span class="material-symbols-outlined">
+pending_actions
+</span>
+ </div>
+  <div className="logintitle">
+  We're sorry, but it seems that your account is already verified. You can log in now.
+</div>
+</div>
+) : (
+  // Display something else when the error message is different
+  <p>An error occurred during verification. Please try again or contact support.</p>
+)}
+      
+      
+
+  
+    </div>;
   } else {
     content = <p>Verification status unknown.</p>;
   }
@@ -69,7 +123,7 @@ const Emailverificationdone = () => {
     <div>
    <Login typeofpage={'registration'}> 
     <div className="logintitle">
-   
+   <br/>
 {content}
 
       </div>
