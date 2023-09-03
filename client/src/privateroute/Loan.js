6952320da236b7349f1dc23c1976bcc76a29ab64
manlikevee <odahviktor@gmail.com>
2023-09-03 22:20:01 +0100

    vcvc

--- client/src/privateroute/Loan.js
+++ client/src/privateroute/Loan.js
@@ -35,7 +35,29 @@ const Loan = () => {
       });
   }, []);
   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get('/Loanpage');
+        // Handle the response as needed
 
+        setResponseData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        // Fail silently without showing errors
+        console.error('Fetch error (silently ignored)', error);
+      }
+    };
+  
+    // Fetch data initially
+    fetchData();
+  
+    // Fetch data every 10 seconds
+    const intervalId = setInterval(fetchData, 30000);
+  
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
 
 
   const [isModalOpen, setModalOpen] = useState(false);
