8598b23325d3edbbb9c61bf6d9b7a234995845d9
Crux <olam4461@gmail.com>
2023-09-04 11:30:43 +0100

    bd

--- client/src/service/axiosinterceptor.js
+++ client/src/service/axiosinterceptor.js
@@ -1,54 +1,69 @@
 import axios from 'axios';
-import axiosAuthRefresh from 'axios-auth-refresh';
 import jwtDecode from 'jwt-decode';
 import dayjs from 'dayjs';
 import { toast } from 'react-toastify';
-import { setUser, logout, getUser } from './auth'; // Replace with your auth functions
+import { setUser, logout } from './auth';
+import { getUser } from './auth';
 import { navigate } from 'gatsby';
 
-const baseURL = 'https://isslblog.vercel.app'; // Your baseURL
+
+const baseURL = 'https://isslblog.vercel.app';
+
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
 });
 
-// Function to refresh the token
-const refreshAuthLogic = async (failedRequest) => {
-  const user = getUser(); // Get user information
+axiosInstance.interceptors.request.use(async (req) => {
+  const user = getUser();
 
-  if (user && user.refreshtoken) {
-    try {
-      const response = await axios.post(`${baseURL}/api/token/refresh/`, {
-        refresh: user.refreshtoken,
-      });
-      const { access, refresh } = response.data;
-      const decodedAccessToken = jwtDecode(access);
-      user.id = decodedAccessToken.user_id;
-      user.username = decodedAccessToken.username;
-      user.email = decodedAccessToken.email;
-      user.accesstoken = access;
-      user.refreshtoken = refresh;
-      window.localStorage.setItem('gatsbyUser', JSON.stringify(user));
-      failedRequest.response.config.headers['Authorization'] = `Bearer ${access}`;
-      return Promise.resolve();
-    } catch (error) {
-      console.error('Failed to refresh access token:', error);
-      toast.error('Session has expired. Please log in again.', {
-        position: toast.POSITION.BOTTOM_CENTER,
-      });
-      logout();
-      navigate('/Userlogin/');
-      // Call the logout function if token refresh fails
-      return Promise.reject(error);
+  if (user && user.accesstoken) {
+    console.log(user.accesstoken);
+    console.log(user.refreshtoken);
+    const decodedAccessToken = jwtDecode(user.accesstoken);
+    const isExpired = dayjs.unix(decodedAccessToken.exp).diff(dayjs()) < 1;
+
+    if (!isExpired) {
+      req.headers.Authorization = `Bearer ${user.accesstoken}`;
+      console.log('not expired');
+      console.log(user.refreshtoken);
+    } else {
+      let retryCount = 0;
+      while (retryCount < 2) { // Try up to 2 times
+
+        try {
+          const response = await axios.post(`${baseURL}/api/token/refresh/`, {
+            refresh: user.refreshtoken,
+          });
+
+          console.log('expired');
+          const { access, refresh } = response.data;
+          const decodedAccessToken = jwtDecode(access);
+          user.id = decodedAccessToken.user_id;
+          user.username = decodedAccessToken.username;
+          user.email = decodedAccessToken.email;
+          user.accesstoken = access;
+          user.refreshtoken = refresh;
+          window.localStorage.setItem('gatsbyUser', JSON.stringify(user));
+          req.headers.Authorization = `Bearer ${access}`;
+          break; // Break out of the retry loop if successful
+        } catch (error) {
+          console.error('Failed to refresh access token:', error);
+          retryCount++;
+          if (retryCount >= 2) {
+            toast.success('Session Expired', {
+              position: toast.POSITION.BOTTOM_CENTER,
+            });
+            logout();
+            navigate('/Userlogin/');
+            throw error;
+          }
+        }
+      }
     }
-  } else {
-    return Promise.reject('No refresh token available');
   }
-};
 
-// Attach the token refresh logic to Axios instance
-axiosAuthRefresh(axiosInstance, refreshAuthLogic, {
-  statusCodes: [401], // Define the status code(s) that trigger token refresh
+  return req;
 });
 
 export default axiosInstance;
