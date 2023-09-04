73b669709764762a2d7608fa8e3c2a5196a98534
manlikevee <odahviktor@gmail.com>
2023-09-05 00:08:37 +0100

    cc

--- client/src/pages/FullScreenComponent.js
+++ client/src/pages/FullScreenComponent.js
@@ -2,54 +2,52 @@ import React, { useEffect, useState } from 'react';
 import Dashboardlayout from '../components/Dashboard/Dashboardlayout'
 
 function FullScreenComponent() {
-  const [isFullScreen, setIsFullScreen] = useState(false);
-
-  const toggleFullScreen = () => {
-    if (!isFullScreen) {
-      // Enter full-screen mode
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-      } else if (document.documentElement.mozRequestFullScreen) {
-        document.documentElement.mozRequestFullScreen();
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen();
-      } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen();
-      }
-    } else {
-      // Exit full-screen mode
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      }
-    }
-
-    // Toggle the full-screen state
-    setIsFullScreen(!isFullScreen);
-  };
-
-  useEffect(() => {
-    // Function to handle F11 key press
-    const handleKeyPress = (e) => {
-      if (e.key === 'F11') {
-        toggleFullScreen();
-      }
-    };
-
-    // Add event listener for keydown
-    window.addEventListener('keydown', handleKeyPress);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  }, [isFullScreen]);
-
+    const [isFullScreen, setIsFullScreen] = useState(
+        sessionStorage.getItem('isFullScreen') === 'true' || false
+      );
+    
+      // Update sessionStorage whenever the isFullScreen state changes
+      useEffect(() => {
+        sessionStorage.setItem('isFullScreen', isFullScreen.toString());
+      }, [isFullScreen]);
+    
+      // Function to toggle full-screen mode
+      const toggleFullScreen = () => {
+        if (!isFullScreen) {
+          enterFullScreen();
+        } else {
+          exitFullScreen();
+        }
+      };
+    
+      // Function to enter full-screen mode
+      const enterFullScreen = () => {
+        if (document.documentElement.requestFullscreen) {
+          document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+          document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+          document.documentElement.msRequestFullscreen();
+        }
+        setIsFullScreen(true);
+      };
+    
+      // Function to exit full-screen mode
+      const exitFullScreen = () => {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+        setIsFullScreen(false);
+      };
+    
   return (
     <Dashboardlayout>
     <div>
