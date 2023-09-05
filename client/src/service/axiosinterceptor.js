657718f5e242303c128713e55482d2d81a6b27d9
manlikevee <odahviktor@gmail.com>
2023-09-05 01:11:43 +0100

    VEE

--- client/src/service/axiosinterceptor.js
+++ client/src/service/axiosinterceptor.js
@@ -6,9 +6,7 @@ import { setUser, logout } from './auth';
 import { getUser } from './auth';
 import { navigate } from 'gatsby';
 
-
 const baseURL = 'https://isslblog.vercel.app';
-
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
@@ -18,25 +16,19 @@ axiosInstance.interceptors.request.use(async (req) => {
   const user = getUser();
 
   if (user && user.accesstoken) {
-    console.log(user.accesstoken);
-    console.log(user.refreshtoken);
     const decodedAccessToken = jwtDecode(user.accesstoken);
     const isExpired = dayjs.unix(decodedAccessToken.exp).diff(dayjs()) < 1;
 
     if (!isExpired) {
       req.headers.Authorization = `Bearer ${user.accesstoken}`;
-      console.log('not expired');
-      console.log(user.refreshtoken);
     } else {
       let retryCount = 0;
       while (retryCount < 2) { // Try up to 2 times
-
         try {
           const response = await axios.post(`${baseURL}/api/token/refresh/`, {
             refresh: user.refreshtoken,
           });
 
-          console.log('expired');
           const { access, refresh } = response.data;
           const decodedAccessToken = jwtDecode(access);
           user.id = decodedAccessToken.user_id;
@@ -51,10 +43,17 @@ axiosInstance.interceptors.request.use(async (req) => {
           console.error('Failed to refresh access token:', error);
           retryCount++;
           if (retryCount >= 2) {
-            toast.success('Error Fetching Data Kindly Refresh', {
-              position: toast.POSITION.BOTTOM_CENTER,
-            });
-            throw error;
+            if (error.response && error.response.data && error.response.data.detail === 'Token is blacklisted') {
+              // Token is blacklisted, log the user out
+              toast.error('session expired')
+              logout();
+              navigate('/Userlogin/');
+            } else {
+              toast.error('Error Fetching Data Kindly Refresh', {
+                position: toast.POSITION.BOTTOM_CENTER,
+              });
+              throw error;
+            }
           }
         }
       }
